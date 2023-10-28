093b0a2526c4f4b22c7c04e0e182824d12e7738d
Fardin <Fardinwi136@gmail.com>
2023-10-28 15:57:57 +0600

    testing

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import pyodbc
 
 app = Flask(__name__)
 
-# create a database conncection
+# first lets create a database connection
 conn_str = (
     r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
     r"DBQ=C:\SafesmartBusinessEn\Database\Property2.mdb;"
@@ -13,20 +13,23 @@ conn_str = (
 connection = pyodbc.connect(conn_str)
 cursor = connection.cursor()
 
-# Create a route to retrieve event records with employee details in JSON format
+
+# at first create a route to retrieve event records with employee details in JSON format
 @app.route("/eventRecord")
 def get_eventRecord():
     try:
-        # Retrieve event records with employee details and department details
+        # retrive all the event records with employee details and department details
         cursor.execute(
             """
-            SELECT E.EmployeeID, E.EmployeeName, E.Code, E.DepartmentID, A.AriseTime, A.EventType, D.DepartmentName, C.CardLow
-            FROM ((Employee AS E
+            SELECT E.EmployeeID, E.EmployeeName, E.Code, E.DepartmentID, A.AriseTime, A.EventType, D.DepartmentName, C.CardLow, CO.CommString, CO.CtrlName, CO.CtrlID
+            FROM (((Employee AS E
             INNER JOIN EventRecord AS A ON E.EmployeeID = A.EmployeeID)
             INNER JOIN Department AS D ON E.DepartmentID = D.DepartmentID)
-            LEFT JOIN Card AS C ON E.EmployeeID = C.EmployeeID
+            LEFT JOIN Card AS C ON E.EmployeeID = C.EmployeeID)
+            LEFT JOIN Controller AS CO ON A.CtrlID = CO.CtrlID
             """
         )
+
         event_rows = cursor.fetchall()
 
         # Create a dictionary to store employee data
@@ -40,21 +43,27 @@ def get_eventRecord():
                     "Code": row.Code,
                     "DepartmentID": row.DepartmentID,
                     "DepartmentName": row.DepartmentName,
-                    "Events": []
+                    "Events": [],
                 }
-            employees[employee_id]["Events"].append({
-                "Date": str(row.AriseTime.date()),
-                "Time": str(row.AriseTime.time()),
-                "EventType": row.EventType,
-                "CardLow": row.CardLow,
-            })
+            employees[employee_id]["Events"].append(
+                {
+                    "Date": str(row.AriseTime.date()),
+                    "Time": str(row.AriseTime.time()),
+                    "EventType": row.EventType,
+                    "CardLow": row.CardLow,
+                    "CtrlID": row.CtrlID,
+                    "CommString": row.CommString,
+                    "CtrlName": row.CtrlName,
+                }
+            )
 
-        # Convert the dictionary into a list
+        # now convert the dictionary into a list
         employee_list = list(employees.values())
 
         return jsonify(employee_list)
     except Exception as e:
         return str(e)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
